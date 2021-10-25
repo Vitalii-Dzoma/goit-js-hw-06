46c13a72a421e3115e0fe3a5d82a0173c3dac3f5
Vitalii-Dzoma <vitoos900@gmail.com>
2021-10-25 11:46:51 +0300

    Работа над ошибками 2

--- src/js/task-06.js
+++ src/js/task-06.js
@@ -3,16 +3,10 @@ const inputEl = document.querySelector('#validation-input')
 
 
 const lostFocusChange = () => {
-    console.log("Потерял фокус")
-    for (let i = 0; i < inputEl.value.length; i += 1) {
-        console.log(inputEl.dataset.length - 1)
-        console.log(i)
         if ( inputEl.value.length - 1 === inputEl.dataset.length - 1) {
             return inputEl.classList.remove('invalid'), inputEl.classList.add('valid');
-        }
-      
+        }  
       return inputEl.classList.add('invalid'), inputEl.classList.remove('valid')  
-    }
 
 }
     inputEl.addEventListener('blur', lostFocusChange)
--- src/js/task-08.js
+++ src/js/task-08.js
@@ -1,24 +1,21 @@
 const formEl = document.querySelector('.login-form')
-
+const inEl = document.querySelectorAll('input')
+const inputEmail = document.querySelector('[type="email"]')
+const inputPassword = document.querySelector('[type="password"]')
 let result = {};
 const formSubmit = (event) => {
     event.preventDefault()
-    
-  
-    for (let el of formEl.elements) {
-        if (el.type === "submit") {
-   continue
-}
-        const eMailEl = el.name;
-        result[eMailEl] = el.value;     
-        if (!el.value) {
-            alert("Необходимо заполнить все поля!!!")
-          
-        }
-        
-}
-       console.log(result)
 
+    const eMailEl = inputEmail.name;
+    const passEl = inputPassword.name;
+    result[eMailEl] = inputEmail.value;
+    result[passEl] = inputPassword.value;
+    if (!inputEmail.value || !inputPassword.value) {
+            alert("Необходимо заполнить все поля!!!")        
+    }
+    else {
+console.log(result)
+    }
  formEl.reset()
 
 }
